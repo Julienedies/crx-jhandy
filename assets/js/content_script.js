bb92ddc466ddf5945af075d69d196831577c81d3
Julien <julienedies@gmail.com>
2015-06-05 23:00:56 +0800

    new menu

--- assets/js/content_script.js
+++ assets/js/content_script.js
@@ -1,83 +1,84 @@
 /**
- * 
+ *
  * 注入当前页面的js脚本；
- * 
+ *
  */
 
-////////////////////////////////////////////////////////
+
+// 接收content script发过来的消息
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+
+    console.log('收到background.js发来的消息', request, sender);
+    call(request.id);
+
+});
+
+
 //
-////////////////////////////////////////////////////////
+function call(id) {
 
-	var _cc = function(){
-		var args = Array.prototype.slice.call(arguments,0);
-		console.log.apply(console, args)
-	}
-	
-	
-	// 接收content script发过来的消息
-	chrome.extension.onRequest.addListener( function(request, sender, sendResponse) { 
-		
-		_cc('收到background.js发来的消息', request, sender);
-		call();
-		
-	});	
+    var each = {
 
+        '33': function each33(i) {
 
+            var red = [];
+
+            var row = $(this);
+            var cells = row.find('td');
+            var num = cells.eq(1).text() * 1;
+
+            cells.filter('.z_font_red').each(function (i) {
+                red.push($(this).text() * 1);
+            });
 
 
-////////////////////////////////////////////////////////
-//
-////////////////////////////////////////////////////////
+            var blue = cells.filter('.z_font_ls').text() * 1;
 
-function call(){
-	
-	var result = [];
+            var dobj = {num: num, red: red, blue: blue};
 
-	var list = $('#info tr');
-	
-	list.each(function(i){
-		
-		var red = [];
-		
-		var row = $(this);
-		var cells = row.find('td'); 
-		var num = cells.eq(1).text() * 1;
-		
-		cells.filter('.z_font_red').each(function(i){
-			red.push( $(this).text() * 1 );
-		});
-		
-		
-		var blue = cells.filter('.z_font_ls').text() * 1;
-		
-		var dobj = {num: num, red: red, blue: blue};
-		
-		result.push(dobj);
-		
-	});
-	
-	
-	
-	_cc(result);
-	
-	
-	$.ajax({
-		type:'post',
-		url: 'http://localhost/tool/ng.php',
-		data: JSON.stringify(result)
-	}).done(function(){
-		_cc('It\'s ok!');
-	});
-	
-	
+            result.push(dobj);
+
+        },
+        '15': function each15(i) {
+
+                var row = $(this);
+                var cells = row.find('td');
+                var num = cells.eq(1).text() * 1;
+                var red = cells.eq(2).text().split(' ');
+
+                red.forEach(function (v, i, arr) {
+                    arr[i] = v * 1;
+                });
+
+                result.push({num: num, red: red});
+            }
+    };
+
+
+    /////////////////////////////////////////////////////////
+
+    var result = [];
+
+    var list = $('#info tr');
+
+    list.each(each[id || 33]);
+
+    console.log(JSON.stringify(result));
+
+
+    $.ajax({
+        type: 'post',
+        url: id ? 'http://localhost:2017/gather/' + id : 'http://localhost:2014',
+        data: JSON.stringify(result),
+        contentType: 'application/json'
+    }).fail(function (err) {
+        alert('err');
+    }).done(function (data) {
+        console.log(data);
+    });
+
+
 }
-
-
-
-
-
-
-
 
 
 
