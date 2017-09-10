310742e969c3c380030a6c1c0494ffabe7d9fc27
julienedis <julienedies@gmail.com>
2017-09-10 17:36:46 +0800

    optimize for back-top

--- js/cs/back-top.js
+++ js/cs/back-top.js
@@ -3,9 +3,13 @@
  */
 
 var div = document.createElement('div');
-div.style.cssText = 'position:fixed;bottom:10px;right:10px;width:30px;height:30px;background-color:rgba(0,0,0,0.4);color:white;text-align:center;line-height:30px;cursor: pointer;';
+div.style.cssText = 'z-index:10001;position:fixed;bottom:10px;right:10px;width:30px;height:30px;background-color:rgba(0,0,0,0.4);color:white;text-align:center;line-height:30px;cursor: pointer;';
+div.title = '单价向上，双击向下';
 div.appendChild(document.createTextNode('^'));
 div.onclick = function(){
     document.documentElement.scrollTop = document.body.scrollTop = 0;
+};
+div.ondblclick = function(){
+    document.body.scrollTop = document.body.scrollHeight;
 };
 document.body.appendChild(div);
