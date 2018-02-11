0baa14a526e7b89f15780526a348a9c66654483a
julienedis <julienedies@gmail.com>
2018-02-11 15:19:39 +0800

    optimize

--- js/cs/fix-baidu-stock.js
+++ js/cs/fix-baidu-stock.js
@@ -3,6 +3,9 @@
  * 百度股票页面样式调整。
  */
 
-document.documentElement.scrollTop = 96;
+document.documentElement.scrollTop = 310;
 
-$('#main-chart-tab li:eq(2)').click();
+//$('#main-chart-tab li:eq(2)').click();
+
+$('[data-id="KKE_tab_kd"]').click();
+$('.kke_cfg_fullscreen').click();
--- js/cs/reset-stock-link.js
+++ js/cs/reset-stock-link.js
@@ -10,10 +10,12 @@ $(document.body).on('click', 'a[href^=https://shuo.taoguba.com.cn/stockBarWeibo?
     var that = e.target;
     if(!that._href){
         var code = that.href.match(/[^=]{8}$/)[0];
-        that._href = 'https://gupiao.baidu.com/stock/sh601989.html'.replace(/[^/]{8}(?=\.html$)/i,code);
+        //'https://www.futunn.com/quote/stock?m=sz&code=002898' //富途股票页面
+        //that._href = 'https://gupiao.baidu.com/stock/sh601989.html'.replace(/[^/]{8}(?=\.html$)/i,code); //百度股票页面
+        that._href = 'http://finance.sina.com.cn/realstock/company/sh600519/nc.shtml'.replace(/[^/]{8}(?=\/nc\.shtml$)/i,code); //新浪股票页面
     }
 
-    if(w){
+    if(w && !w.closed){
         w.location.href = that._href;
         w.focus();
     }else{
