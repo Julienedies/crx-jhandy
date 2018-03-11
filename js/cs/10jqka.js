e04df07883b2a7e3182b7c97be67e862124c5566
julienedis <julienedies@gmail.com>
2018-03-11 17:28:30 +0800

    optimize

--- js/cs/10jqka.js
+++ js/cs/10jqka.js
@@ -10,14 +10,14 @@ var index = STOCK_CODE.indexOf(current);
 var prev = index - 1 < 0 ? STOCK_CODE.length - 1 : index - 1;
 var next = index + 1 > STOCK_CODE.length - 1 ? 0 : index + 1;
 
-console.log(index, prev, current , next);
+console.log(index, prev, current, next);
 
 $(document.body).on('mousewheel', function (e) {
 
     //正负值表示滚动方向
     var isUp = e.originalEvent.deltaY < 0;
 
-    var href =  location.href.replace(reg, '/' + ( isUp ? STOCK_CODE[prev] : STOCK_CODE[next] ) + '/');
+    var href = location.href.replace(reg, '/' + ( isUp ? STOCK_CODE[prev] : STOCK_CODE[next] ) + '/');
 
     console.log(href);
 
