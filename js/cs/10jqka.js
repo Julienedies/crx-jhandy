5f4fe9c1a0939600dc5f2b9a9a2a426c2f37a9ba
julienedis <julienedies@gmail.com>
2018-04-28 12:53:46 +0800

    optimzie

--- js/cs/10jqka.js
+++ js/cs/10jqka.js
@@ -5,20 +5,20 @@
  */
 
 //alert('I am 10jqka.js.');
-
-STOCK_CODE = STOCK_CODE.map(function (x) {
+STOCKS = STOCKS || [];
+STOCKS = STOCKS.map(function (x) {
     return x[0];
 });
 var reg = /\/(\d{6})\//;
 var current = location.href.match(reg)[1];
-var index = STOCK_CODE.indexOf(current);
-var prev = index - 1 < 0 ? STOCK_CODE.length - 1 : index - 1;
-var next = index + 1 > STOCK_CODE.length - 1 ? 0 : index + 1;
+var index = STOCKS.indexOf(current);
+var prev = index - 1 < 0 ? STOCKS.length - 1 : index - 1;
+var next = index + 1 > STOCKS.length - 1 ? 0 : index + 1;
 
 //发送消息给background.js，通过background.js同步个股K线页面
-//if(STOCK_CODE.indexOf(current) > -1){
+if(STOCKS.indexOf(current) > -1){
     chrome.runtime.sendMessage({id: '10jqka', code: current});
-//}
+}
 
 var href = location.href;
 var url = /company.html$/.test(href) ? href.replace('company.html', '') : href + 'company.html';
@@ -36,7 +36,7 @@ if (/^http:\/\/basic\.10jqka\.com.cn\/\d{6}\/?$/img.test(location.href)) {
         //正负值表示滚动方向
         var isUp = e && e.originalEvent.deltaY < 0;
 
-        var href = location.href.replace(reg, '/' + ( isUp ? STOCK_CODE[prev] : STOCK_CODE[next] ) + '/');
+        var href = location.href.replace(reg, '/' + ( isUp ? STOCKS[prev] : STOCKS[next] ) + '/');
 
         console.log(href);
 
