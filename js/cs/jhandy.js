057c6e00f163f6f474eb631cecc5e4956fde85dd
julienedis <julienedies@gmail.com>
2018-08-30 21:54:47 +0800

    optimize

--- js/cs/jhandy.js
+++ js/cs/jhandy.js
@@ -4,6 +4,17 @@
 
 console.log('I am jhandy.js');
 
+// 是否股市开盘时间
+function is_on_the_exchange(){
+    var d = new Date();
+    var z = d.getDay();
+    var h = d.getHours();
+    if( z > 0 && z < 6 && h > 8 && h < 15) {
+        return true;
+    }
+    return false;
+}
+
 chrome.runtime.onMessage.addListener(function (msg) {
 
     console.log(msg);
@@ -12,12 +23,7 @@ chrome.runtime.onMessage.addListener(function (msg) {
 
     if (e == 'active_ftnn') {
 
-        let d = new Date();
-        let h = d.getHours();
-
-        if(h > 8 && h < 15) {
-            $('#active_ftnn').click();
-        }
+        is_on_the_exchange() && $('#active_ftnn').click();
 
     } else if (e == 'view_in_tdx') {
         $('#tdx_view').attr('code', msg.code).click();
