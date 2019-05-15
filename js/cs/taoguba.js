05ef81cb16a99725663bdad76b0e355fd512ec88
julienedis <julienedies@gmail.com>
2019-05-15 09:11:07 +0800

    optimize

--- js/cs/taoguba.js
+++ js/cs/taoguba.js
@@ -9,15 +9,15 @@ let w; //新窗口引用。
 
 const isReadInfo = location.pathname === '/readInfo';  // 比赛实盘页面
 
-function view_in_tdx(code) {
+function view_in_tdx (code) {
     chrome.runtime.sendMessage({todo: 'socket', event: 'view_in_tdx', code: code.replace(/[szh]/img, '')});
 }
 
-function view_in_ftnn(code) {
+function view_in_ftnn (code) {
     chrome.runtime.sendMessage({todo: 'socket', event: 'view_in_ftnn', code: code.replace(/[szh]/img, '')});
 }
 
-function fix_links() {
+function fix_links () {
     let $th = $(this);
     let str = $th.text();
     console.log(str);
@@ -28,7 +28,7 @@ function fix_links() {
             if (STOCK_LIST[i][0] == code) {
                 console.log(code);
                 $th.text(code + '(*)'.replace('*', STOCK_LIST[i][1]));
-                isReadInfo && $th.after(`<a href="view_in_ftnn/${code}">ftnn</a> `);
+                isReadInfo && $th.after(`<a href="view_in_ftnn/${ code }">ftnn</a> `);
                 return;
             }
         }
@@ -48,54 +48,53 @@ $(document.body).on('click', 'a[href^=view_in_]', function (e) {
     console.log(that.href, arr, code, event);
     chrome.runtime.sendMessage({event: event, code: code});
     return false;
-}).
-    on('click', 'a[href*=https://www.taoguba.com.cn/quotes/], a[href^="/public/static/img/p/"]', function (e) {
-        var that = e.target;
-        var code = that.href.match(/\w{2}\d{6}(?!\d)/)[0];
-        console.log(code);
-        chrome.runtime.sendMessage({event: 'view_in_tdx', code: code.replace(/[szh]/img, '')});
-        return false;
+}).on('click', 'a[href*=https://www.taoguba.com.cn/quotes/], a[href^="/public/static/img/p/"]', function (e) {
+    var that = e.target;
+    var code = that.href.match(/\w{2}\d{6}(?!\d)/)[0];
+    console.log(code);
+    chrome.runtime.sendMessage({event: 'view_in_tdx', code: code.replace(/[szh]/img, '')});
+    return false;
 
-        if (!that._href) {
-            if (location.host === "127.0.0.1:3300") {
-                code = that.href.match(/[^/]{6}(?=\.png)/i)[0];
-                code = /^6\d{5}$/.test(code) ? 'sh' + code : 'sz' + code;
-            } else {
-                code = that.href.match(/\w{2}\d{6}(?!\d)/)[0];
-                //chrome.runtime.sendMessage({event: 'tdx_view', code: code.replace(/[szh]*/img,'')});
-            }
-
-            //富途股票页面
-            //'https://www.futunn.com/quote/stock?m=sz&code=002898';
-            //百度股票页面
-            //that._href = 'https://gupiao.baidu.com/stock/sh601989.html'.replace(/[^/]{8}(?=\.html$)/i,code);
-            //新浪股票页面
-            //that._href = 'http://finance.sina.com.cn/realstock/company/sh600519/nc.shtml'.replace(/[^/]{8}(?=\/nc\.shtml$)/i,code);
-            //雪球
-            that._href = 'https://xueqiu.com/S/' + code;
-        }
-
-        if (w && !w.closed) {
-            w.location.href = that._href;
-            w.focus();
+    if (!that._href) {
+        if (location.host === "127.0.0.1:3300") {
+            code = that.href.match(/[^/]{6}(?=\.png)/i)[0];
+            code = /^6\d{5}$/.test(code) ? 'sh' + code : 'sz' + code;
         } else {
-            w = window.open(that._href);
+            code = that.href.match(/\w{2}\d{6}(?!\d)/)[0];
+            //chrome.runtime.sendMessage({event: 'tdx_view', code: code.replace(/[szh]*/img,'')});
         }
 
-        return false;
-    });
+        //富途股票页面
+        //'https://www.futunn.com/quote/stock?m=sz&code=002898';
+        //百度股票页面
+        //that._href = 'https://gupiao.baidu.com/stock/sh601989.html'.replace(/[^/]{8}(?=\.html$)/i,code);
+        //新浪股票页面
+        //that._href = 'http://finance.sina.com.cn/realstock/company/sh600519/nc.shtml'.replace(/[^/]{8}(?=\/nc\.shtml$)/i,code);
+        //雪球
+        that._href = 'https://xueqiu.com/S/' + code;
+    }
+
+    if (w && !w.closed) {
+        w.location.href = that._href;
+        w.focus();
+    } else {
+        w = window.open(that._href);
+    }
+
+    return false;
+});
 
 
 // ------------------------------------------------------------------------- 滚动到页面底部自动加载更多 start
 
-if(location.href === 'https://www.taoguba.com.cn/getMoreListAction') {
+if (location.href.includes('https://www.taoguba.com.cn/getMoreListAction')) {
 
     let $loadMore = $('#clickLoadmore');
     let cb = () => {
         $loadMore.click();
     };
 
-    function onScrollEnd(callback){
+    function onScrollEnd (callback) {
 
         let clientHeight = $(window).height();
         let $doc = $(document);
