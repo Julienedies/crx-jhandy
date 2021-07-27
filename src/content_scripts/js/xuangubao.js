b9a6c88c68b3d5f9a30d955c1140e2ac347e6548
julienedis <julienedies@gmail.com>
2021-07-27 19:26:31 +0800

    optimize

--- src/content_scripts/js/xuangubao.js
+++ src/content_scripts/js/xuangubao.js
@@ -9,9 +9,17 @@ import utils from '../../js/lib/utils'
 
 console.log('I am xuangubao.js.');
 
+// 如果当前页面是选股宝首页，每分钟刷新一次
+if(location.href === 'https://xuangubao.cn/'){
+    console.log('刷新', +new Date);
+    setInterval( () => {
+        location.reload();
+    }, 45 * 1000);
+}
 
-let $loadmore = $('.home-news-footer-loadmore');
+
+/*let $loadmore = $('.home-news-footer-loadmore');
 
 utils.onScrollEnd(() => {
     $loadmore.length && $loadmore[0].click();
-});
+});*/
