ef0758955e1e8cb595fa9c586012e76105261309
julienedis <julienedies@gmail.com>
2021-10-23 20:03:18 +0800

    optimize

--- src/content_scripts/js/on-select.js
+++ src/content_scripts/js/on-select.js
@@ -12,7 +12,8 @@ import onSelectHtml from './on-select.html';
 
 console.log('I am on-select.js.');
 
-const shandyHost = 'https://localhost:3301';
+//const shandyHost = 'https://localhost:3301';
+const shandyHost = 'http://localhost:3300';
 const $doc = $(document);
 
 let urlKey = utils.createUrlKey();
