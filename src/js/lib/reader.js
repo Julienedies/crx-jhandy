f2b54adbe7d3d7eea5c5ca5e3de89e301de70359
julienedis <julienedies@gmail.com>
2021-11-08 22:47:23 +0800

    optimize

--- src/js/lib/reader.js
+++ src/js/lib/reader.js
@@ -25,7 +25,7 @@ class Reader {
             #reader-control-wrapper{
                 position:fixed;
                 z-index:10000;
-                top:0;
+                bottom:0;
                 left:0;
                 right:0;
                 background:rgba(0,0,0,0.7);
